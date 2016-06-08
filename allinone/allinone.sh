4b64675dc97035fd849d016d7a733247d0b4d080
root <root@wreese-aio.rackfs.com>
2016-06-08 19:22:42 +0000

    More allinone cleanup

--- allinone/allinone.sh
+++ allinone/allinone.sh
@@ -85,6 +85,18 @@ TENDOT=$(ifconfig | awk -F "[: ]+" '/inet addr:/ { if ($4 != "127.0.0.1") print 
 sed -e "s/TENDOTME/$TENDOT/g" /etc/syndicate/cfssl/localhost.json.tmpl | sed -e "s/HOSTNAMEME/`hostname -f`/g" > /etc/syndicate/cfssl/localhost.json
 cfssl gencert -ca=/etc/syndicate/cfssl/ca.pem -ca-key=/etc/syndicate/cfssl/ca-key.pem -config=/etc/syndicate/cfssl/ca-config.json -profile=client-server /etc/syndicate/cfssl/localhost.json | cfssljson -bare localhost
 
+echo "fixing rings"
+DUMMY_NODEID=`syndicate-client search | awk '/ID/{print $2}' | head -n1`
+NODEID=`syndicate-client search | awk '/ID/{print $2}' | tail -n1`
+syndicate-client capacity $NODEID 1000
+syndicate-client active $NODEID true
+syndicate-client rm $DUMMY_NODEID
+DUMMY_NODEID=`syndicate-client -addr 127.0.0.1:8444 search | awk '/ID/{print $2}' | head -n1`
+NODEID=`syndicate-client -addr 127.0.0.1:8444 search | awk '/ID/{print $2}' | tail -n1`
+syndicate-client -addr 127.0.0.1:8444 capacity $NODEID 1000
+syndicate-client -addr 127.0.0.1:8444 active $NODEID true
+syndicate-client -addr 127.0.0.1:8444 rm $DUMMY_NODEID
+
 echo
 echo "!! Don't forget to remove the place holder nodes from the ring once you've started your nodes"
 echo
