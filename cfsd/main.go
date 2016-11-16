966c74a64bc2d80466893f9d5dd220a365a38f40
Greg Holt <gregory.holt@gmail.com>
2016-11-16 05:26:49 -0600

    Working towards cfsd

---
+++ cfsd/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/gholt/ring"
+	"golang.org/x/net/context"
+)
+
+const (
+	ADDR_FORMIC = iota
+	ADDR_GROUP_GRPC
+	ADDR_GROUP_REPL
+	ADDR_VALUE_GRPC
+	ADDR_VALUE_REPL
+)
+
+func main() {
+	// TODO: Completely missing all formic stuff
+
+	fp, err := os.Open("/etc/cfsd/cfs.ring")
+	if err != nil {
+		panic(err)
+	}
+	oneRing, err := ring.LoadRing(fp)
+	if err != nil {
+		panic(err)
+	}
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		panic(err)
+	}
+FIND_LOCAL_NODE:
+	for _, addrObj := range addrs {
+		if ipNet, ok := addrObj.(*net.IPNet); ok {
+			for _, node := range oneRing.Nodes() {
+				for _, nodeAddr := range node.Addresses() {
+					i := strings.LastIndex(nodeAddr, ":")
+					if i < 0 {
+						continue
+					}
+					nodeIP := net.ParseIP(nodeAddr[:i])
+					if ipNet.Contains(nodeIP) {
+						oneRing.SetLocalNode(node.ID())
+						break FIND_LOCAL_NODE
+					}
+				}
+			}
+		}
+	}
+
+	waitGroup := &sync.WaitGroup{}
+	shutdownChan := make(chan struct{})
+
+	groupStore, groupStoreRestartChan, err := NewGroupStore(&GroupStoreConfig{
+		GRPCAddressIndex: ADDR_GROUP_GRPC,
+		ReplAddressIndex: ADDR_GROUP_REPL,
+		CertFile:         "/etc/cfsd/cert.pem",
+		KeyFile:          "/etc/cfsd/cert-key.pem",
+		CAFile:           "/etc/cfsd/ca.pem",
+		Scale:            0.4,
+		Path:             "/mnt/cfsd",
+		Ring:             oneRing,
+	})
+	waitGroup.Add(1)
+	go func() {
+		for {
+			select {
+			case <-groupStoreRestartChan:
+				ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+				groupStore.Shutdown(ctx)
+				ctx, _ = context.WithTimeout(context.Background(), time.Minute)
+				groupStore.Startup(ctx)
+			case <-shutdownChan:
+				ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+				groupStore.Shutdown(ctx)
+				waitGroup.Done()
+				return
+			}
+		}
+	}()
+	ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+	if err = groupStore.Startup(ctx); err != nil {
+		ctx, _ = context.WithTimeout(context.Background(), time.Minute)
+		groupStore.Shutdown(ctx)
+		panic(err)
+	}
+
+	valueStore, valueStoreRestartChan, err := NewValueStore(&ValueStoreConfig{
+		GRPCAddressIndex: ADDR_VALUE_GRPC,
+		ReplAddressIndex: ADDR_VALUE_REPL,
+		CertFile:         "/etc/cfsd/cert.pem",
+		KeyFile:          "/etc/cfsd/cert-key.pem",
+		CAFile:           "/etc/cfsd/ca.pem",
+		Scale:            0.4,
+		Path:             "/mnt/cfsd",
+		Ring:             oneRing,
+	})
+	waitGroup.Add(1)
+	go func() {
+		for {
+			select {
+			case <-valueStoreRestartChan:
+				ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+				valueStore.Shutdown(ctx)
+				ctx, _ = context.WithTimeout(context.Background(), time.Minute)
+				valueStore.Startup(ctx)
+			case <-shutdownChan:
+				ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+				valueStore.Shutdown(ctx)
+				waitGroup.Done()
+				return
+			}
+		}
+	}()
+	ctx, _ = context.WithTimeout(context.Background(), time.Minute)
+	if err = valueStore.Startup(ctx); err != nil {
+		ctx, _ = context.WithTimeout(context.Background(), time.Minute)
+		valueStore.Shutdown(ctx)
+		panic(err)
+	}
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	waitGroup.Add(1)
+	go func() {
+		for {
+			select {
+			case <-ch:
+				fmt.Println("Shutting down due to signal")
+				close(shutdownChan)
+				waitGroup.Done()
+				return
+			case <-shutdownChan:
+				waitGroup.Done()
+				return
+			}
+		}
+	}()
+
+	fmt.Println("Done launching components")
+	waitGroup.Wait()
+}
---
+++ cfsd/package.go
@@ -0,0 +1,5 @@
+package main
+
+// got is at https://github.com/gholt/got
+//go:generate got store.got groupstore_GEN_.go TT=GROUP T=Group t=group
+//go:generate got store.got valuestore_GEN_.go TT=VALUE T=Value t=value
