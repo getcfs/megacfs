7b9ae1f651f81063c1348895a48b8cc61fa1f774
Greg Holt <gregory.holt@gmail.com>
2016-12-03 08:33:58 -0600

    Updated cfsadm/cfsd to support 2 ips

--- cfsadm/main.go
+++ cfsadm/main.go
@@ -15,61 +15,96 @@ import (
 )
 
 func main() {
+	help := `
+init        Creates a new CA, conf file, and initial ring builder file
+add <ip>    Adds the IP to the ring, one for all services, and creates a new
+            certificate for it
+add <public_ip> <private_ip>
+            Same as the single IP add, but the first IP is just for Formic (the
+            public facing API service) and the other IP is for the rest.
+`
 	args := os.Args[1:]
 	if len(args) == 0 {
-		args = []string{"help"}
+		fmt.Println(help)
+		os.Exit(1)
 	}
-	for i := 0; i < len(args); i++ {
-		switch args[i] {
-		default:
-			fmt.Println("init       Creates a new CA, conf file, and initial ring builder file")
-			fmt.Println("add <ip>   Adds the IP to the ring and creates a new certificate for it")
+	switch args[0] {
+	default:
+		fmt.Println(help)
+		os.Exit(1)
+	case "init":
+		if len(args) > 1 {
+			fmt.Println("'init' takes no arguments")
+			fmt.Println(help)
 			os.Exit(1)
-		case "init":
-			if err := os.Mkdir("/etc/cfsd", 0700); err != nil && !os.IsExist(err) {
+		}
+		if err := os.Mkdir("/etc/cfsd", 0700); err != nil && !os.IsExist(err) {
+			panic(err)
+		}
+		fp, err := os.OpenFile("/etc/cfsd/cfsd.conf", os.O_CREATE|os.O_EXCL|os.O_RDWR, 0600)
+		if err != nil {
+			if !os.IsExist(err) {
 				panic(err)
 			}
-			fp, err := os.OpenFile("/etc/cfsd/cfsd.conf", os.O_CREATE|os.O_EXCL|os.O_RDWR, 0600)
-			if err != nil {
-				if !os.IsExist(err) {
-					panic(err)
-				}
-			} else {
-				fp.Write([]byte("AUTH_URL=http://localhost:5000\nAUTH_USER=admin\nAUTH_PASSWORD=admin\n"))
-				fp.Close()
+		} else {
+			fp.Write([]byte("AUTH_URL=http://localhost:5000\nAUTH_USER=admin\nAUTH_PASSWORD=admin\n"))
+			fp.Close()
+		}
+		for _, name := range []string{"/etc/cfsd/ca.pem", "/etc/cfsd/ca-key.pem", "/etc/cfsd/cfs.builder", "/etc/cfsd/cfs.ring"} {
+			if _, err := os.Lstat(name); err == nil {
+				panic(name + " already exists")
 			}
-			for _, name := range []string{"/etc/cfsd/ca.pem", "/etc/cfsd/ca-key.pem", "/etc/cfsd/cfs.builder", "/etc/cfsd/cfs.ring"} {
+		}
+		cert, _, key, err := initca.New(&csr.CertificateRequest{
+			CN:         "CA",
+			KeyRequest: &csr.BasicKeyRequest{A: "rsa", S: 2048},
+		})
+		if err != nil {
+			panic(err)
+		}
+		if err = ioutil.WriteFile("/etc/cfsd/ca.pem", cert, 0664); err != nil {
+			panic(err)
+		}
+		if err = ioutil.WriteFile("/etc/cfsd/ca-key.pem", key, 0600); err != nil {
+			panic(err)
+		}
+		if err = ring.CLI([]string{"cfsadm init", "/etc/cfsd/cfs.builder", "create"}, os.Stdout, false); err != nil {
+			panic(err)
+		}
+	case "add":
+		args = args[1:]
+		var publicIP string
+		var privateIP string
+		if len(args) > 0 {
+			publicIP = args[0]
+			args = args[1:]
+		}
+		if len(args) > 0 {
+			privateIP = args[0]
+			args = args[1:]
+		}
+		if len(args) > 0 {
+			fmt.Println("too many arguments; syntax: <ip> [ip]")
+			fmt.Println(help)
+			os.Exit(1)
+		}
+		if publicIP == "" {
+			panic("The 'add' command requires the IP address to add.")
+		}
+		ips := []string{publicIP}
+		if privateIP != "" {
+			ips = append(ips, privateIP)
+		} else {
+			privateIP = publicIP
+		}
+		for _, ip := range ips {
+			for _, name := range []string{"/etc/cfsd/" + ip + ".pem", "/etc/cfsd/" + ip + "-key.pem"} {
 				if _, err := os.Lstat(name); err == nil {
 					panic(name + " already exists")
 				}
 			}
-			cert, _, key, err := initca.New(&csr.CertificateRequest{
-				CN:         "CA",
-				KeyRequest: &csr.BasicKeyRequest{A: "rsa", S: 2048},
-			})
-			if err != nil {
-				panic(err)
-			}
-			if err = ioutil.WriteFile("/etc/cfsd/ca.pem", cert, 0664); err != nil {
-				panic(err)
-			}
-			if err = ioutil.WriteFile("/etc/cfsd/ca-key.pem", key, 0600); err != nil {
-				panic(err)
-			}
-			if err = ring.CLI([]string{"cfsadm init", "/etc/cfsd/cfs.builder", "create"}, os.Stdout, false); err != nil {
-				panic(err)
-			}
-		case "add":
-			i++
-			if i >= len(args) {
-				panic("The 'add' command requires the IP address to add.")
-			}
-			ipAddr := args[i]
-			for _, name := range []string{"/etc/cfsd/" + ipAddr + ".pem", "/etc/cfsd/" + ipAddr + "-key.pem"} {
-				if _, err := os.Lstat(name); err == nil {
-					panic(name + " already exists")
-				}
-			}
+		}
+		for _, ip := range ips {
 			csrBytes, key, err := (&csr.Generator{
 				Validator: genkey.Validator,
 			}).ProcessRequest(&csr.CertificateRequest{
@@ -95,32 +130,32 @@ func main() {
 			}
 			cert, err := signerr.Sign(signer.SignRequest{
 				Request: string(csrBytes),
-				Hosts:   []string{ipAddr},
+				Hosts:   []string{ip},
 			})
 			if err != nil {
 				panic(err)
 			}
-			if err = ioutil.WriteFile("/etc/cfsd/"+ipAddr+".pem", cert, 0664); err != nil {
+			if err = ioutil.WriteFile("/etc/cfsd/"+ip+".pem", cert, 0664); err != nil {
 				panic(err)
 			}
-			if err = ioutil.WriteFile("/etc/cfsd/"+ipAddr+"-key.pem", key, 0600); err != nil {
+			if err = ioutil.WriteFile("/etc/cfsd/"+ip+"-key.pem", key, 0600); err != nil {
 				panic(err)
 			}
-			replicaCount := 3
-			if _, b, err := ring.RingOrBuilder("/etc/cfsd/cfs.builder"); err != nil || b == nil {
-				panic(err)
-			} else if len(b.Nodes())+1 < replicaCount {
-				replicaCount = len(b.Nodes()) + 1
-			}
-			if err = ring.CLI([]string{"cfsadm add", "/etc/cfsd/cfs.builder", "set-replicas", fmt.Sprintf("%d", replicaCount)}, os.Stdout, false); err != nil {
-				panic(err)
-			}
-			if err = ring.CLI([]string{"cfsadm add", "/etc/cfsd/cfs.builder", "add", "address0=" + ipAddr + ":12300", "address1=" + ipAddr + ":12310", "address2=" + ipAddr + ":12311", "address3=" + ipAddr + ":12320", "address4=" + ipAddr + ":12321"}, os.Stdout, false); err != nil {
-				panic(err)
-			}
-			if err = ring.CLI([]string{"cfsadm add", "/etc/cfsd/cfs.builder", "ring"}, os.Stdout, false); err != nil {
-				panic(err)
-			}
+		}
+		replicaCount := 3
+		if _, b, err := ring.RingOrBuilder("/etc/cfsd/cfs.builder"); err != nil || b == nil {
+			panic(err)
+		} else if len(b.Nodes())+1 < replicaCount {
+			replicaCount = len(b.Nodes()) + 1
+		}
+		if err := ring.CLI([]string{"cfsadm add", "/etc/cfsd/cfs.builder", "set-replicas", fmt.Sprintf("%d", replicaCount)}, os.Stdout, false); err != nil {
+			panic(err)
+		}
+		if err := ring.CLI([]string{"cfsadm add", "/etc/cfsd/cfs.builder", "add", "address0=" + publicIP + ":12300", "address1=" + privateIP + ":12310", "address2=" + privateIP + ":12311", "address3=" + privateIP + ":12320", "address4=" + privateIP + ":12321"}, os.Stdout, false); err != nil {
+			panic(err)
+		}
+		if err := ring.CLI([]string{"cfsadm add", "/etc/cfsd/cfs.builder", "ring"}, os.Stdout, false); err != nil {
+			panic(err)
 		}
 	}
 }
