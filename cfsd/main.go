8c7a3ed3c8bfc4816fce0adc228b28bfbe60233b
Chuck Thier <cthier@gmail.com>
2016-12-07 20:28:09 +0000

    Initial pulling ENV based config in for formic
    Also added AUTH config items
    AUTH_URL
    AUTH_USER
    AUTH_PASSWORD

--- cfsd/main.go
+++ cfsd/main.go
@@ -11,17 +11,10 @@ import (
 	"time"
 
 	"github.com/getcfs/megacfs/formic"
-	pb "github.com/getcfs/megacfs/formic/proto"
-	"github.com/getcfs/megacfs/ftls"
-	"github.com/getcfs/megacfs/oort/api"
 	"github.com/getcfs/megacfs/oort/api/server"
 	"github.com/gholt/ring"
-	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
-	"github.com/spaolacci/murmur3"
 	"github.com/uber-go/zap"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 const (
@@ -155,64 +148,24 @@ FIND_LOCAL_NODE:
 		logger.Fatal("Error starting value store", zap.Error(err))
 	}
 
-	creds, err := credentials.NewServerTLSFromFile(certPath, keyPath)
+	// Startup formic
+	err = formic.NewFormicServer(&formic.Config{
+		FormicAddressIndex: ADDR_FORMIC,
+		ValueAddressIndex:  ADDR_VALUE_GRPC,
+		GroupAddressIndex:  ADDR_GROUP_GRPC,
+		CertFile:           certPath,
+		KeyFile:            keyPath,
+		CAFile:             caPath,
+		Ring:               oneRing,
+		RingPath:           ringPath,
+		IpAddr:             ipAddr,
+		AuthUrl:            "http://localhost:5000",
+		AuthUser:           "admin",
+		AuthPassword:       "admin",
+	}, logger)
 	if err != nil {
-		logger.Fatal("Couldn't load cert from file", zap.Error(err))
+		logger.Fatal("Couldn't start up formic", zap.Error(err))
 	}
-	s := grpc.NewServer(
-		grpc.Creds(creds),
-		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
-		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
-	)
-
-	oortLogger := baseLogger.With(zap.String("name", "cfsd.formic.oort"))
-	vstore := api.NewReplValueStore(&api.ValueStoreConfig{
-		Logger:       oortLogger,
-		AddressIndex: ADDR_VALUE_GRPC,
-		StoreFTLSConfig: &ftls.Config{
-			MutualTLS: true,
-			CertFile:  certPath,
-			KeyFile:   keyPath,
-			CAFile:    caPath,
-		},
-		RingCachePath: ringPath,
-		RingClientID:  ipAddr,
-	})
-
-	gstore := api.NewReplGroupStore(&api.GroupStoreConfig{
-		Logger:       oortLogger,
-		AddressIndex: ADDR_GROUP_GRPC,
-		StoreFTLSConfig: &ftls.Config{
-			MutualTLS: true,
-			CertFile:  certPath,
-			KeyFile:   keyPath,
-			CAFile:    caPath,
-		},
-		RingCachePath: ringPath,
-		RingClientID:  ipAddr,
-	})
-
-	comms, err := formic.NewStoreComms(vstore, gstore, logger)
-	if err != nil {
-		logger.Fatal("Error setting up comms", zap.Error(err))
-	}
-	deleteChan := make(chan *formic.DeleteItem, 1000)
-	dirtyChan := make(chan *formic.DirtyItem, 1000)
-	fs := formic.NewOortFS(comms, logger, deleteChan, dirtyChan)
-	deletes := formic.NewDeletinator(deleteChan, fs, comms, baseLogger.With(zap.String("name", "formic.deletinator")))
-	cleaner := formic.NewCleaninator(dirtyChan, fs, comms, baseLogger.With(zap.String("name", "formic.cleaninator")))
-	go deletes.Run()
-	go cleaner.Run()
-
-	l, err := net.Listen("tcp", oneRing.LocalNode().Address(ADDR_FORMIC))
-	if err != nil {
-		logger.Fatal("Failed to bind formic to port", zap.Error(err))
-	}
-	pb.RegisterFileSystemAPIServer(s, formic.NewFileSystemAPIServer(gstore, vstore, baseLogger.With(zap.String("name", "formic.fs"))))
-	formicNodeID := int(murmur3.Sum64([]byte(ipAddr)))
-	pb.RegisterApiServer(s, formic.NewApiServer(fs, formicNodeID, comms, logger))
-	logger.Info("Starting formic and the filesystem API", zap.Int("addr", formicNodeID))
-	go s.Serve(l)
 
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
