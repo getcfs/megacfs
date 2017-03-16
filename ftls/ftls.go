5804790eda847b619061e54499e5896de9c9f676
Greg Holt <gregory.holt@gmail.com>
2017-03-16 17:15:58 -0500

    Cleaned up ftls

--- ftls/ftls.go
+++ ftls/ftls.go
@@ -1,3 +1,5 @@
+// Package ftls provides simpler/default ways to use TLS, including Mutual TLS,
+// with CFS.
 package ftls
 
 import (
@@ -11,14 +13,7 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-var (
-	DefaultMinVersion = uint16(tls.VersionTLS12)
-	DefaultCipherSet  = []uint16{tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384}
-)
-
-type Ftsl struct {
-	c *Config
-}
+var DefaultMinVersion = uint16(tls.VersionTLS12)
 
 type Config struct {
 	StrictClientVerify bool
@@ -31,36 +26,35 @@ type Config struct {
 	CAFile             string
 }
 
-// DefaultServerFTLSConf returns a ftls config with the most commonly used config set.
+// DefaultServerFTLSConf returns a ftls config with the most commonly used
+// server config set.
 func DefaultServerFTLSConf(CertFile, KeyFile, CAFile string) *Config {
 	return &Config{
 		StrictClientVerify: true,
 		MutualTLS:          true,
 		InsecureSkipVerify: false,
-		// TODO: I know syn wanted these minimums, but they weren't working last I tested.
-		// MinVersion:         DefaultMinVersion,
-		// CipherSet:          DefaultCipherSet,
-		CertFile: CertFile,
-		KeyFile:  KeyFile,
-		CAFile:   CAFile,
+		MinVersion:         DefaultMinVersion,
+		CertFile:           CertFile,
+		KeyFile:            KeyFile,
+		CAFile:             CAFile,
 	}
 }
 
-// DefaultClientFTLSConf returns a ftls config with the most commonly used config set.
+// DefaultClientFTLSConf returns a ftls config with the most commonly used
+// client config set.
 func DefaultClientFTLSConf(CertFile, KeyFile, CAFile string) *Config {
 	return &Config{
 		MutualTLS:          true,
 		InsecureSkipVerify: false,
-		// TODO: I know syn wanted these minimums, but they weren't working last I tested.
-		// MinVersion:         DefaultMinVersion,
-		CertFile: CertFile,
-		KeyFile:  KeyFile,
-		CAFile:   CAFile,
+		CertFile:           CertFile,
+		KeyFile:            KeyFile,
+		CAFile:             CAFile,
 	}
 }
 
-// NewClientTLSConfig constructs a client tls.Conf from provide ftls Config.
-func NewClientTLSConfig(c *Config) (*tls.Config, error) {
+// newClientTLSConfig constructs a client tls.Conf from the provided ftls
+// Config.
+func newClientTLSConfig(c *Config) (*tls.Config, error) {
 	clientCertPool := x509.NewCertPool()
 	if c.CAFile != "" {
 		clientCACert, err := ioutil.ReadFile(c.CAFile)
@@ -86,7 +80,8 @@ func NewClientTLSConfig(c *Config) (*tls.Config, error) {
 	return &tls.Config{RootCAs: clientCertPool, InsecureSkipVerify: c.InsecureSkipVerify}, nil
 }
 
-// NewServerTLSConfig constructs a server tls.Conf from the provided ftls Config.
+// NewServerTLSConfig constructs a server tls.Conf from the provided ftls
+// Config.
 func NewServerTLSConfig(c *Config) (*tls.Config, error) {
 	tlsConf := &tls.Config{}
 	if c.MutualTLS {
@@ -107,7 +102,7 @@ func NewServerTLSConfig(c *Config) (*tls.Config, error) {
 			ClientCAs:                clientCertPool,
 			CipherSuites:             c.CipherSet,
 			PreferServerCipherSuites: true,
-			MinVersion:               DefaultMinVersion,
+			MinVersion:               c.MinVersion,
 		}
 		tlsConf.BuildNameToCertificate()
 	}
@@ -120,19 +115,26 @@ func NewServerTLSConfig(c *Config) (*tls.Config, error) {
 	return tlsConf, nil
 }
 
+// NewGRPCClientDialOpt provides a GRPC option for TLS configuration using the
+// provided ftls Config.
 func NewGRPCClientDialOpt(c *Config) (grpc.DialOption, error) {
 	var opt grpc.DialOption
 	if c.InsecureSkipVerify {
 		return grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})), nil
 	}
-	tlsConf, err := NewClientTLSConfig(c)
+	tlsConf, err := newClientTLSConfig(c)
 	if err != nil {
 		return opt, err
 	}
 	return grpc.WithTransportCredentials(credentials.NewTLS(tlsConf)), nil
 }
 
-func VerifyClientAddrMatch(c *tls.Conn) error {
+// verifyClientAddrMatch ensures the client certificate in use matches the
+// client IP address.
+//
+// NOTE: This isn't in use now because GRPC handles it for us, but I'm leaving
+// it in case we need the code later.
+func verifyClientAddrMatch(c *tls.Conn) error {
 	err := c.Handshake()
 	if err != nil {
 		return err
